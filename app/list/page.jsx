4b011ea9b32363d098788f12c69f7394bf7e7edc
hey-ri <kwonhr01@naver.com>
2023-06-07 22:45:09 +0900

    Get db and display

---
+++ app/list/page.jsx
@@ -0,0 +1,20 @@
+import { connectDB } from '@/util/database';
+
+export default async function List() {
+  const client = await connectDB;
+  const db = client.db('forum');
+  const result = await db.collection('post').find().toArray();
+  console.log(result);
+  return (
+    <div className="list-bg">
+      {result.map((data, i) => {
+        return (
+          <div className="list-item">
+            <h4>{data.title}</h4>
+            <p>1월 1일</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import { MongoClient } from 'mongodb';
 import { connectDB } from '@/util/database';
 
 export default async function Home() {

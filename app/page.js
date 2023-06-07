b02080114aefc4f93e307845c9b6746d15c11193
hey-ri <kwonhr01@naver.com>
2023-06-07 22:31:38 +0900

    add a description

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 import { connectDB } from '@/util/database';
 
 export default async function Home() {
-  /* DB와 연결하는 코드들은 server component 안에다가 작성하는 것이 좋다. */
+  /* DB와 연결하는 코드들은 server component 안에다가 작성하는 것이 좋다. client에 적으면 유저가 쉽게 볼 수 있기 때문이다 */
   const client = await connectDB;
   const db = client.db('forum');
   const result = await db.collection('post').find().toArray();

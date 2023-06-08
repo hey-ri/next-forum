116f1f8ae5d40885c24793fe4070554e2be37fbe
hey-ri <kwonhr01@naver.com>
2023-06-08 22:02:18 +0900

    create go to the each other page

--- app/list/page.jsx
+++ app/list/page.jsx
@@ -1,15 +1,17 @@
 import { connectDB } from '@/util/database';
+import Link from 'next/link';
 
 export default async function List() {
   const client = await connectDB;
   const db = client.db('forum');
   const result = await db.collection('post').find().toArray();
   console.log(result);
+
   return (
     <div className="list-bg">
       {result.map((data, i) => (
         <div key={i} className="list-item">
-          <h4>{data.title}</h4>
+          <Link href={`/detail/${data._id}`}>{data.title}</Link>
           <p>1월 1일</p>
         </div>
       ))}

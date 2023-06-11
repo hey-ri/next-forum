e8c8e7dc870797a67d5f917b2a44b2c01d5c2db4
hey-ri <kwonhr01@naver.com>
2023-06-11 22:18:12 +0900

    modify if statement

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -8,8 +8,8 @@ export default async function handler(req, res) {
       const result = db.collection('post').insertOne(req.body);
 
       res.redirect(302, '/list');
-    } else {
-      res.status(500).json('제목 및 글 내용은 빈칸이 될 수 없습니다');
+      return;
     }
+    res.status(500).json('제목 및 글 내용은 빈칸이 될 수 없습니다');
   }
 }

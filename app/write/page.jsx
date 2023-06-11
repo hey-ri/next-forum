1f81bba37f719eb1a56514ab9aed993efb18072c
hey-ri <kwonhr01@naver.com>
2023-06-11 22:16:50 +0900

    user send the content, resonse the result

--- app/write/page.jsx
+++ app/write/page.jsx
@@ -1,8 +1,10 @@
 export default function Write() {
   return (
-    <div>
+    <div className="p-20">
       <h4>글작성</h4>
-      <form action="/api/testServer" method="POST">
+      <form action="/api/post/new" method="POST">
+        <input type="text" name="name" placeholder="제목을 입력하세요" />
+        <input type="text" name="title" placeholder="내용을 입력하세요" />
         <button type="submit">보내기</button>
       </form>
     </div>
---
+++ pages/api/post/new.js
@@ -0,0 +1,15 @@
+import { connectDB } from '@/util/database';
+
+export default async function handler(req, res) {
+  if (req.method == 'POST') {
+    // console.log(req.body);
+    if (req.body.title !== '' && req.body.content !== '') {
+      const db = (await connectDB).db('forum');
+      const result = db.collection('post').insertOne(req.body);
+
+      res.redirect(302, '/list');
+    } else {
+      res.status(500).json('제목 및 글 내용은 빈칸이 될 수 없습니다');
+    }
+  }
+}

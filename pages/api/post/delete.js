0aa6ff05b09f6ea75d8cf48159c13a502af6d390
hey-ri <kwonhr01@naver.com>
2023-06-17 23:46:16 +0900

    edit delete feature
    but can't delete animation

--- pages/api/post/delete.js
+++ pages/api/post/delete.js
@@ -2,13 +2,12 @@ import { connectDB } from '@/util/database';
 import { ObjectId } from 'mongodb';
 
 export default async function handler(req, res) {
-  if (req.method == 'POST' || req.method == 'DELETE') {
-    console.log('delete 페이지', req.body);
-
+  console.log('delete 페이지', req.body);
+  if (req.method == 'POST') {
     try {
       const db = (await connectDB).db('forum');
-      const result = db.collection('post').deleteOne({ _id: new ObjectId(req.body) });
-      console.log(result);
+      const result = await db.collection('post').deleteOne({ _id: new ObjectId(req.body) });
+      console.log('결과', result);
     } catch (err) {
       res.status(500);
     }

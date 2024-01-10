76dd9d16fafec75649246f0c38ff6564c13fb9c2
hey-ri <kwonhr01@naver.com>
2024-01-10 17:21:47 +0900

    feat: 이미 있는 이메일, 이메일&&비밀번호&&이름에 관하여 빈칸이면 회원가입 안되게

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,12 +2,20 @@ import { connectDB } from '@/util/database';
 import bcrypt from 'bcrypt';
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
+  const db = (await connectDB).db('forum');
+  const alreadyData = await db.collection('user_cred').find().toArray();
+  // const alreadyData = await db.collection('user_cred').findOne({ email: req.user.email });
+
+  console.log(alreadyData.map((result) => result.email).includes(req.body.email));
+  if (alreadyData.map((result) => result.email).includes(req.body.email)) {
+    res.status(500).json('해당 이메일은 이미 존재합니다.');
+  } else if (req.method === 'POST' && req.body.password !== '' && req.body.email !== '' && req.body.name !== '') {
     const hash = await bcrypt.hash(req.body.password, 10);
     req.body.password = hash;
 
-    let db = (await connectDB).db('forum');
     await db.collection('user_cred').insertOne(req.body);
     res.status(200).json('성공');
+  } else {
+    res.status(500).json('해당 칸에 빈칸이 있으면 안됩니다.');
   }
 }

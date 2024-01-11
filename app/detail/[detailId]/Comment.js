55ff353ad4ea27d24773cb56889721bd33b41aca
hey-ri <kwonhr01@naver.com>
2024-01-11 12:06:56 +0900

    feat: 상세페이지에 입력한 댓글 출력

--- app/detail/[detailId]/Comment.js
+++ app/detail/[detailId]/Comment.js
@@ -1,13 +1,23 @@
 'use client';
 
-import {  useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Comment(props) {
   const [comments, setComments] = useState('');
+  const [commentList, setCommentList] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/comment/list?id=' + props.parentId)
+      .then((r) => r.json())
+      .then((result) => {
+        console.log(result);
+        setCommentList(result);
+      });
+  }, []);
 
   return (
     <div>
-      <div>댓글 목록 보여줄 부분</div>
+      <div>{commentList.length > 0 ? commentList.map((d, i) => <p key={i}>{d.comment}</p>) : '댓글을 작성 해보세요'}</div>
       <input onChange={(e) => setComments(e.target.value)} />
       <button
         onClick={() => {

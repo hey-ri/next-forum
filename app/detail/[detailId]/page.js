17b9d614364d3e20c1a4da3709fa35d027e776fa
hey-ri <kwonhr01@naver.com>
2024-01-16 09:56:38 +0900

    feat: not-found파일 생성 및 삽입

--- app/detail/[detailId]/page.js
+++ app/detail/[detailId]/page.js
@@ -1,13 +1,17 @@
 import { connectDB } from '@/util/database';
 import { ObjectId } from 'mongodb';
 import Comment from './Comment';
+import { notFound } from 'next/navigation';
 
 /* 다이나믹 라우트 []로 이름을 감싸준다 /detail/1 /detail/2 detail/3 등등의 detail/ 다음에 아무거나 올 수 있게, 그 루트로 url로 페이지 접근이 가능하다 */
 async function Detail(props) {
   const client = await connectDB;
   const db = client.db('forum');
   const result = await db.collection('post').findOne({ _id: new ObjectId(props.params.detailId) });
-  // console.log('상세페이지 id', props.params.detailId);
+  if (!result) {
+    return notFound();
+  }
+
   console.log(result);
   return (
     <div>
